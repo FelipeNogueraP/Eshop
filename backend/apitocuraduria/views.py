07f60aeff5fbd3cdb8cf9fb8327d5eb8d4232ae0
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-04-23 15:57:45 -0500

    add rest framework to settings

--- backend/apitocuraduria/views.py
+++ backend/apitocuraduria/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.Response import Response
 
 # Create your views here.
+
+@api_view
+def getRoutes(request):
+    routes = [
+    'api/products/',
+
+    ]
+    return Response(routes)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #My apps 
-    
+    #My apps
+
     'apitocuraduria.apps.ApitocuraduriaConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
