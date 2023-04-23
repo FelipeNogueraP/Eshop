1e230a031613c9ce876242236782cb1d161dca41
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-04-23 17:16:57 -0500

    add products and initial apis to url

--- backend/apitocuraduria/urls.py
+++ backend/apitocuraduria/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    path('products/', views.getProducts, name="products"),
+    path('product/<str:pk>', views.getProduct, name="product"),
 
 
 ]
--- backend/apitocuraduria/views.py
+++ backend/apitocuraduria/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .products import products
 from rest_framework.decorators import api_view
-from rest_framework.Response import Response
+from rest_framework.response import Response
 
 # Create your views here.
 
-@api_view
+
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
-    'api/products/',
+        'api/products/',
+        'api/products/create',
+        'api/products/upload',
+        'api/products/<id>/reviews/',
+        'api/products/top/',
+        'api/products/<id>/',
+        'api/products/delete/<id>/',
+        'api/products/<update>/<id>/',
+
 
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getProducts(request):  # Get all Products
+    return Response(products)
+
+
+@api_view(['GET', 'POST'])
+def getProduct(request, pk):  # Get a single route
+    product = None
+    for elem in products:
+        if elem['_id'] == pk:
+            product = elem
+            break
+    return Response(product)
