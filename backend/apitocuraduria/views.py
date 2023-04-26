9c892580904ecd6ca177fb9f024648b6d3bf6d65
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-04-25 19:55:41 -0500

    user serializer

--- backend/apitocuraduria/views.py
+++ backend/apitocuraduria/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .products import products
+from .models import Product
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Product
-from .products import products
-from .serializers import ProductSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 # Create your views here.
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer = UserSerializerWithToken(self.user).data
+        
+        for key, values in serializer.items():
+            data[key] = values
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
@@ -28,15 +45,19 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+def getUserProfile(request):  
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
 def getProducts(request):  # Get all Products
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
-
 
 @api_view(['GET'])
 def getProduct(request, pk):  # Get a single Product
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
- 
