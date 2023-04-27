d694081cf6dee0bc20eb5261a59732dd2f49c45c
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-04-27 17:02:00 -0500

    backend user authentication

--- backend/apitocuraduria/urls/order_urls.py
+++ backend/apitocuraduria/urls/order_urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from apitocuraduria.views import order_views as views
 
 urlpatterns = [
-    
+    path('add/', views.addOrderItems, name='orders-add'),
+    path('myorders/', views.getMyOrders, name='myorders'),
+    path('<str:pk>/', views.getOrderById, name='user-order'),
+    path('<str:pk>/pay/', views.updateOrderToPaid, name='pay'),
 ]
