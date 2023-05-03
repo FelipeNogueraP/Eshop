a664fdb817a307e24f808035daa5039acc8ec4f7
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-05-02 20:11:50 -0500

    add frontend til Rating component

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import HomeScreen from "./screens/HomeScreen";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
