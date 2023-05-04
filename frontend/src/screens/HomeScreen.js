4fd2a4bdfc8ffc4596c7e6de4cbf5a988917985a
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-05-04 15:16:30 -0500

    fetching data betwen django and react servers

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
-import products from "../products";
+// import products from "../products";
 import Product from "../components/Product";
+import axios from "axios";
 
 function Homescreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products/");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import products from "../products";
+import axios from "axios";
 
 function ProductScreen() {
+  const [product, setProduct] = useState(null);
   const { id } = useParams();
-  const product = products.find((p) => String(p._id) === id);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [id]);
 
   if (!product) return null;
 
