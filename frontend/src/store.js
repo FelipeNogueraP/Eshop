8cda18b058fd2216788d77ba210d97f1b8b0bd03
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-05-05 17:40:15 -0500

    change name of repo

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({});
-  
+
 const initialState = {};
 
 const middleware = [thunk];
