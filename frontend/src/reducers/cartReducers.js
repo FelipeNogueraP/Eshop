e20ae83203122cf5108ceef4f338a9d700e42aaa
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-05-10 19:23:12 -0500

    cart reducer and action

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,29 @@
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+    switch (action.type) {
+        case CART_ADD_ITEM:
+            // console.log(action.payload)
+            const item = action.payload
+            // console.log(item)
+            const existItem = state.cartItems.find(x => x.product === item.product)
+            // console.log(existItem)
+            if (existItem) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(x => x.product === existItem.product ? item : x)
+                }
+
+            }
+            else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item]
+                }
+            }
+        default:
+            return state
+    }
+}
+
+
