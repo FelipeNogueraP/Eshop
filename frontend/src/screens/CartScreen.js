8ad1718ed501750840cd9d75fc11f750dcc32182
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-05-11 19:46:11 -0500

    add to cart functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
 function CartScreen() {
-  return (
-    <div>CartScreen</div>
-  )
+  const { id } = useParams();
+  const location = useLocation();
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const navigate = useNavigate();
+
+  console.log('cartItems: ', cartItems);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]);
+
+  // const removeFromCartHandler = (id) => {
+  //   console.log("remove");
+  // };
+
+  // const checkoutHandler = () => {
+  //   navigate.push("/login?redirect=shipping");
+  // };
+
+  return <div>Cart</div>;
 }
 
-export default CartScreen
+export default CartScreen;
