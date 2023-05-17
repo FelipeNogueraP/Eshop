6050734141ac0b9c4b00edaa6faeca24cd05b500
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-05-17 10:08:45 -0500

    checkout steps

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
 import { saveShippingAddress } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 function ShippingScreen() {
   const cart = useSelector((state) => state.cart);
@@ -23,6 +24,7 @@ function ShippingScreen() {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
 
