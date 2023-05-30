21d747ae5bf3ba20fca286f29439bb2b7515c32d
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-05-30 15:33:09 -0500

    get order by id

--- backend/apitocuraduria/views/order_views.py
+++ backend/apitocuraduria/views/order_views.py
@@ -68,23 +68,25 @@ def getMyOrders(request):
     return Response(serializer.data)
 
 
-@api_view(['POST'])
+@api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getOrderById(request, pk):
     user = request.user
 
     try:
-        order = Order.objects.get(id=pk)
+        order = Order.objects.get(_id=pk)
 
         if user.is_staff or order.user == user:
             serializer = OrderSerializer(order, many=False)
             return Response(serializer.data)
 
         else:
-            return Response({'detail': 'Not authorized to view this order'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'detail': 'Not authorized to view this order'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
     except:
-        return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'detail': 'Order does not exist'},
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['PUT'])
