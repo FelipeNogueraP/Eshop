68a3e5b0fe1800ecac02922bcb70062231c59aa6
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-06-13 15:09:33 -0500

    Create n update products endpoints

--- backend/apitocuraduria/models.py
+++ backend/apitocuraduria/models.py
@@ -7,13 +7,16 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(
+        null=True, blank=True, default='/sample.jpg')
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    rating = models.DecimalField(max_digits=7, decimal_places=2, blank=True)
+    rating = models.DecimalField(
+        max_digits=7, decimal_places=2, blank=True, null=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
-    price = models.DecimalField(max_digits=7, decimal_places=2, blank=True)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, blank=True, null=True)
     countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
